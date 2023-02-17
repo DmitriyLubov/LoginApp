1ce384f5ef8507b1f38b4bba79f63601a9e2c7ba
DmitriyLubov <snipere@list.ru>
2023-02-17 22:37:53 +1000

    Work with UI

---
+++ LoginApp/ViewControllers/UserTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  UserTabBarController.swift
+//  LoginApp
+//
+//  Created by Дмитрий Лубов on 17.02.2023.
+//
+
+import UIKit
+
+final class UserTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+//        tabBar.backgroundColor = .label
+    }
+}
