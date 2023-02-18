6c687a9187943bf927ef0fc72e6ff74cf9c77d11
DmitriyLubov <snipere@list.ru>
2023-02-18 12:39:04 +1000

    Work with model

---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,35 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Дмитрий Лубов on 18.02.2023.
+//
+
+struct User {
+    let userName: String
+    let password: String
+    
+    let person: Person
+    
+    static func getUser() -> User {
+        User(
+            userName: "User",
+            password: "password",
+            person: Person(
+                name: "Дмитрий",
+                lastname: "Лубов",
+                company: "Артем ИРЦ",
+                department: "Автоматизации",
+                post: "Программист БД"
+            )
+        )
+    }
+}
+
+struct Person {
+    let name: String
+    let lastname: String
+    let company: String
+    let department: String
+    let post: String
+}
